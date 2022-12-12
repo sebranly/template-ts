6374eaece1dbfa6782300aca47497d7ed7882009
sebranly <sebranly.pinf@gmail.com>
2022-12-12 15:44:35 +0100

    feat: add more elements

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Template TypeScript/i);
+  expect(textElement).toBeInTheDocument();
 });
